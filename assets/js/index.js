df5112c933f1b491ce0358f1372544d440bdf9f1
iGeek <jesus.cardenas.d@gmail.com>
2023-09-23 11:37:51 -0600

    ejercicio finalizado

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,27 @@
 console.log("Entro index.js");
+
+
+const btnBlue = document.getElementById("btnBlue");
+
+
+// btnBlue.addEventListener("click", function () {
+//     console.log("Click en el boton azul!!!!");
+// });
+
+function clickBotonAzul() {
+    console.log("Click en el boton azul!!!!");
+}
+
+function overButtonAzul() {
+    alert("Entro a over de boton Azul.");
+}
+
+function changeBackGroundColorPage(color) {
+    document.body.style.backgroundColor = color;
+}
+
+btnBlue.addEventListener("click", ()=> {
+    changeBackGroundColorPage("blue")
+});
+
+btnBlue.addEventListener("mouseover", overButtonAzul);
